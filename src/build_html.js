85fff0d9306642fa31bf0a23cdd3b3a6abc059a3
Sebastian Wallat <wallatsebastian@gmail.com>
2020-11-02 10:37:10 +0100

    added bootstrap-select and applied styling to dropdown

--- src/build_html.js
+++ src/build_html.js
@@ -7,12 +7,14 @@ const scripts = [
   'https://sdk.amazonaws.com/js/aws-sdk-2.779.0.min.js',
   'https://code.jquery.com/jquery-3.2.1.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js',
-  'https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js'
+  'https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js'
 ]
 
 const css = [
   'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
-  '/css/styles.css'
+  '/css/styles.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css'
 ]
 
 function sendHTML (res) {
